b457f97ac2f78e5ea0e648c631a53a4a35dc31b0
CODE-BR3AKER <zarktizakaria@gmail.com>
2021-10-22 14:44:08 -0400

    intial commit

---
+++ _nuxt/static/1612302556/code/chn/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1612302556",layout:"default",error:null,serverRendered:a,routePath:"\u002Fcode\u002FCHN",config:{axios:{browserBaseURL:void 0}},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
